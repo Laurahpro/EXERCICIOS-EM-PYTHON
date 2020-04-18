8e8590992776e5cc6d244c8d13f724d105054023
Laurah <laurah.gna@gmail.com>
2020-04-18 12:55:49 -0300

    Primeira Aula de Python
    Exercícios da Primeira Aula de Python Gustavo Guanabara

---
+++ Ex1Python.py
@@ -0,0 +1,11 @@
+print ('Olá mundo!')
+print ('Variaveis')
+nome = 'Guanabara'
+idade = 25
+peso = 74.8 
+print (nome, idade, peso)
+
+nome = input ('Qual é o seu nome?')
+idade = input ('Qual é a sua idade?')
+peso = input ('Qual é o seu peso?')
+print (nome, idade, peso)
