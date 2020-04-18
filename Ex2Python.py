72b3b020799b869fa7b9777f5f02066927ef0f08
Laurah <laurah.gna@gmail.com>
2020-04-18 13:26:32 -0300

    Exercícios Simples de Python
    Testando variáveis

---
+++ Ex2Python.py
@@ -0,0 +1,8 @@
+nome = input('Nome completo')
+idade = input('Idade')
+serie = input('Série escolar')
+escola = input('Nome da Escola')
+materia = input('Matéria favorita')
+nota1 = input('Sua maior nota na escola')
+nota2 = input('Sua menor nota na escola') 
+print (nome, idade, serie, escola, materia, nota1, nota2) 
