a20904778449077edd6c33e57e45d8568cd04772
Laurah <laurah.gna@gmail.com>
2020-04-24 20:49:00 -0300

    update
    update

---
+++ ex000.py
@@ -0,0 +1,8 @@
+nome = input('Qual o seu nome?')
+print('É um grande prazer te conhecer,', nome)
+idade = input('Quantos anos você tem?')
+print('Bacana que você tem', idade,'anos', nome,'!')
+filho = input('Você tem filhos?')
+print('Que bacana!')
+nomeFilho = input('Qual o nome do seu filho?')
+print('Então ele se chama', nomeFilho, '!')
