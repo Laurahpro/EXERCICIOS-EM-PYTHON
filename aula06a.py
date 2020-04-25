a91fb041dd527ce5186b6d48ff80aa504131bc3e
Laurah <laurah.gna@gmail.com>
2020-04-24 21:09:13 -0300

    Desafio 3
    Crie um programa que leia dois números e mostre a soma entre eles.

--- aula06a.py
+++ aula06a.py
@@ -1,5 +1,5 @@
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro valor: '))
 s = n1 + n2
-# print('A soma entre', n1, 'e ', n2, 'é igual a', s )
+# print('A soma entre', n1, 'e ', n2, 'é igual a', s ) <- formato antigo do Python 
 print('A soma entre {} e {} vale {}'.format(n1, n2, s))
