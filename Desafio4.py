80d7c8ff622f98dd87954e900bc35bbc73343ba8
Laurah <laurah.gna@gmail.com>
2020-04-25 16:45:14 -0300

    Operadores Aritméticos
    Aula de Operadores Aritméticos

--- Desafio4.py
+++ Desafio4.py
@@ -1,4 +1,4 @@
-n1 = input('Digite um número: ')
+n1 = input('Digite algo: ')
 print('É um número?', n1.isnumeric())
 print('É alfanumérico?', n1.isalnum())
-print('É uma letra?',n1.isalpha())
+print('É uma palavra?',n1.isalpha())
---
+++ aula07a.py
@@ -0,0 +1,8 @@
+n1 = int(input('Escreva um número: '))
+n2 = int(input('Escreva outro número: '))
+s = n1 + n2
+m = n1 * n2 
+d = n1 / n2 
+di= n1 //n2
+e = n1**n2
+print('A soma é {}, o produto é {}, a divisão é {}, a divisão inteira é {}, a exponenciação é {}'.format (s, m, d, di, e))
