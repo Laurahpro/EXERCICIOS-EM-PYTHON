a456b39e252c17f6270c8de0222998dde3b566bf
Laurah <laurah.gna@gmail.com>
2020-04-28 20:11:33 -0300

    CORREÇÃO EX5
    Correção do exercício 5

--- Desafio5.py
+++ Desafio5.py
@@ -1,4 +1,4 @@
-n1 =int (input('Escreva um número: '))
-s= n1 - 1 
-su= n1 + 1
-print ('O antecessor do número {} é o número {}, e o que vem depois é o número {}'.format(n1, s, su))
+n =int (input('Escreva um número: '))
+a = n - 1 
+su = n + 1
+print ('O antecessor do número {} é o número {}, e o sucessor é {}'.format(n, (n-1), (n+1)))
