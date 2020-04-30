5cd1fc865173e5eed67657536234bb721d25080d
Laurah <laurah.gna@gmail.com>
2020-04-30 12:25:16 -0300

    Update  ex014
    a ordem de precedência dessa expressão é a mesma da regra, ou seja, nesse caso não precisa de parentesis!

--- ex014.py
+++ ex014.py
@@ -1,3 +1,5 @@
 C = float(input('Informe a temperatura em ºC: '))
 f = ((9 * C) / 5 ) + 32
 print('A temperatura de {}ºC corresponde a {}ºF!'.format(C, f))
+
+# a ordem de precedência dessa expressão é a mesma da regra, ou seja, nesse caso não precisa de parentesis! 
