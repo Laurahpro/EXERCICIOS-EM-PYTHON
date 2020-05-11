575ff96d5d011129b843dd6689ac89254813b60d
Laurah <laurah.gna@gmail.com>
2020-05-11 12:31:03 -0300

    Ex019
    Random

---
+++ ex019.py
@@ -0,0 +1,7 @@
+# ==== Exercício 19 ====
+from random import choice
+alu1 = input('Primeiro aluno: ')
+alu2 = input('Segundo aluno: ')
+alu3 = input('Terceiro aluno: ')
+alu4 = input('Quarto aluno: ')
+print('O aluno selecionado é {}'.format(choice([alu1, alu2, alu3, alu4])))
