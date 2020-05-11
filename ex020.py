0a4921e0707b7544fcf11f9fd56237814fbef4b6
Laurah <laurah.gna@gmail.com>
2020-05-11 12:58:05 -0300

    Ex020
    Sorteio de ordem de apresentação de alunos

---
+++ ex020.py
@@ -0,0 +1,10 @@
+# ==== Exercício 20 ==== 
+from random import choice
+alu1 = input('Primeiro aluno: ')
+alu2 = input('Segundo aluno: ')
+alu3 = input('Terceiro aluno: ')
+alu4 = input('Quarto aluno: ')
+alu5 = input('Quinto aluno: ')
+alu6 = input('Sexto aluno: ')
+print('O aluno seleccionado é {}'.format(choice([alu1, alu2, alu3, alu4])))
+
